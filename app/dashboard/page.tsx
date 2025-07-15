36d0f22a5f34aad9283f7b183918fbdd6576f92a
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-15 15:18:15 -0700

    add search functionality on dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 // import DashboardLayout from './layout';
 import template from '@/utils/template';
 import Image from 'next/image';
@@ -6,6 +7,13 @@ import { Search } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Page() {
+	const [search, setSearch] = useState('');
+	const filteredTemplates = template.filter((item) => {
+		const searchTerm = search.toLowerCase();
+		// Check if the item's name or description includes the search term (case-insensitive)
+		return item.name.toLowerCase().includes(searchTerm);
+		// item.desc.toLowerCase().includes(searchTerm)
+	});
 	return (
 		<div>
 			<div className="p-10 mx-5 mb-5 rounded-lg bg-slate-200 dark:bg-slate-800 flex flex-col justify-center items-center">
@@ -17,6 +25,10 @@ export default function Page() {
 						<Search className="text-primary" />
 						<input
 							type="text"
+							value={search}
+							onChange={(e) => {
+								setSearch(e.target.value);
+							}}
 							placeholder="Search"
 							className="bg-transparent w-full outline-none text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400"
 						/>
@@ -25,7 +37,7 @@ export default function Page() {
 			</div>
 
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
-				{template.map((item) => {
+				{filteredTemplates.map((item) => {
 					return (
 						<Link
 							key={item.slug}
@@ -48,6 +60,11 @@ export default function Page() {
 						</Link>
 					);
 				})}
+				{filteredTemplates.length === 0 && (
+					<p className="col-span-full text-center text-gray-500">
+						No templates found matching your search.
+					</p>
+				)}
 			</div>
 		</div>
 	);
