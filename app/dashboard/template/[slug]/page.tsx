4e44cd4488c2cd03cc54dd5fdb5884a74d14d533
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-15 10:14:23 -0700

    template details

---
+++ app/dashboard/template/[slug]/page.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Link from 'next/link';
+import { ArrowLeft, Divide } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import template from '@/utils/template';
+import Image from 'next/image';
+
+export interface Template {
+	name: string;
+	slug: string;
+	icon: string;
+	desc: string;
+	category: string;
+	aiPrompt: string;
+	form: Form[];
+}
+
+export interface Form {
+	label: string;
+	field: string;
+	name: string;
+	required: boolean;
+}
+export default function page({ params }: { params: { slug: string } }) {
+	const t = template.find((item) => item.slug === params.slug) as Template;
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-5">
+			<div className="col-span-1 bg-slate-100 dark:bg-slate-900 rounded-md border p-5">
+				<div className="flex flex-col gap-3">
+					<Image src={t.icon} alt={t.name} width={50} height={50} />
+					<h2 className="font-medium text-lg">{t.name}</h2>
+					<p className="text-gray-500">{t.desc}</p>
+				</div>
+				<form className="mt-6">
+					{t.form.map((item) => (
+						<div
+							key={item.name}
+							className="my-2 flex flex-col gap-2 mb-7"
+						>
+							<label className="font-bold pb-5">
+								{item.label}
+							</label>
+							{item.field === 'input' ? (
+								<Input
+									name={item.name}
+									required={item.required}
+								/>
+							) : (
+								<Textarea
+									name={item.name}
+									required={item.required}
+								/>
+							)}
+						</div>
+					))}
+				</form>
+			</div>
+		</div>
+	);
+}
