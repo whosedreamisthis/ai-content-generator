d15b3ebceb06447c6eb616bf68e7a4f3428080e5
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-16 20:52:31 -0700

    billing and setting pages placeholders

---
+++ app/dashboard/billing/page.tsx
@@ -0,0 +1,12 @@
+// page.tsx (For History Page)
+'use client';
+import React, { useState, useEffect } from 'react';
+import { getQueries } from '@/actions/ai';
+import { useUser } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+import { Divide, Loader2Icon } from 'lucide-react';
+import QueryTable from '@/components/table/query-table';
+
+export default function Page() {
+	return <div> This is the billing page. </div>;
+}
